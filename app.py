96cd6a41c275be55f1281da6d6b491a3b33bea62
Mohammad Ikmal <111510512+MalSpielplatz@users.noreply.github.com>
2023-06-11 19:37:38 +0700

    Update

--- app.py
+++ app.py
@@ -1,103 +1,45 @@
 import streamlit as st
-from PIL import Image
-from tensorflow.keras.preprocessing.image import load_img, img_to_array, save_img
 import numpy as np
-import shutil
+import matplotlib.pyplot as plt
+from skimage.io import imread
+from skimage.transform import resize
+import pickle
+from PIL import Image
+st.title('Ular berbisa atau bukan ?')
+st.text('Upload Image')
 
-import os
-import random
-import webbrowser
+model = pickle.load(open('model/keras_model.h5','rb'))
 
-#=================================== Title ===============================
-st.title("""
-Berbisa atau tidak ?
-	""")
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+if uploaded_file is not None:
+	img = Image.open(uploaded_file)
+	st.image(img,caption='Uploaded Image')
 
-#================================= Title Image ===========================
-st.text("""""")
+# # Load the model
+# model = load_model('keras_model.h5')
 
-#======================== Time To See The Magic ===========================
-st.write("""
-## 👁️‍🗨️ Time To See The Magic 🌀
-	""")
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-#========================== File Uploader ===================================
-img_file_buffer = st.file_uploader("Upload an image here 👇🏻")
+# Load the image
+img_path = input('Enter the path to your image: ')
+img = imread(img_path)
 
-try:
-    image = Image.open(img_file_buffer)
-    img_array = np.array(image)
-    st.write("""
-        Preview 👀 Of Given Image!
-        """)
-    if image is not None:
-        st.image(
-            image,
-            use_column_width=True
-        )
-    st.write("""
-        Now, you are just one step ahead of prediction.
-        """)
-    st.write("""
-        **Just Click The '👉🏼 Predict' Button To See The Prediction Corresponding To This Image! 😄**
-        """)
-except:
-    st.write("""
-        ### ❗ Any Picture hasn't selected yet!!!
-        """)
+# Resize the image
+img_resized = resize(img, (224, 224, 3))
 
-#================================= Predict Button ============================
-st.text("""""")
-submit = st.button("👉🏼 Predict")
+# Expand dimensions to add the batch size
+img_resized = np.expand_dims(img_resized, axis=0)
 
-#==================================== Model ==================================
-def processing(testing_image_path):
-    IMG_SIZE = 50
-    img = load_img(testing_image_path,
-                   target_size=(IMG_SIZE, IMG_SIZE), color_mode="grayscale")
-    img_array = img_to_array(img)
-    img_array = img_array / 255.0
-    img_array = img_array.reshape((1, 50, 50, 1))
-    prediction = model.predict(img_array)
-    return prediction
+# Make prediction
+y_out = model.predict(img_resized)
+y_out = np.argmax(y_out, axis=1)
 
+# Map the prediction to the respective class
+class_names = ['Non Venomous', 'Venomous']
+predicted_class = class_names[y_out[0]]
 
-def generate_result(prediction):
-    class_names = ['Non Venomous', 'Venomous']
-    prediction = class_names[np.argmax(prediction)]
-    return prediction
-
-
-#=========================== Predict Button Clicked ==========================
-if submit:
-    try:
-        # save image on that directory
-        if not os.path.exists("temp_dir"):
-            os.makedirs("temp_dir")
-
-        save_img("temp_dir/test_image.png", img_array)
-
-        image_path = "temp_dir/test_image.png"
-        # Predicting
-        st.write("👁️ Predicting...")
-
-        model_path_h5 = "model/model.h5"
-        model = tensorflow.keras.models.load_model(model_path_h5)
-
-        prediction = processing(image_path)
-        result = generate_result(prediction)
-
-        st.write(f"Prediction: {result}")
-
-    except:
-        st.write("""
-        ### ❗ Oops... Something Is Going Wrong
-        """)
-
-#=============================== Copy Right ==============================
-st.text("""""")
-st.text("""""")
-st.text("""""")
-st.write("""
-### ©️ Created By awokwaokaowkwao
-    """)
+# Display the image
+plt.imshow(img_resized[0])
+plt.show()
+print(f'PREDICTED OUTPUT: {predicted_class}')
