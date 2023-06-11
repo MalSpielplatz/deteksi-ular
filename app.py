9f4e9386db312df82d9f879bb5a71a2a23bc8ba2
Mohammad Ikmal <111510512+MalSpielplatz@users.noreply.github.com>
2023-06-11 20:19:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,44 +4,76 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 from skimage.io import imread
 from skimage.transform import resize
-import pickle
+import os
 from PIL import Image
+
+
 st.title('Ular berbisa atau bukan ?')
 st.text('Upload Image')
-st.text("MEKI")
+
+
+a = np.array([[1, 2, 3, 4, 5],
+              [4, 5, 6, 7, 8]])
+
+a.flatten()
+
+target = []
+images = []
+flat_data = []
+
+DATADIR = 'Snake'
+CATEGORIES = ['Non Venomous','Venomous']
+
+for category in CATEGORIES:
+    class_num = CATEGORIES.index(category)
+    path = os.path.join(DATADIR, category)
+    for img in os.listdir(path):
+        img_array = imread(os.path.join(path, img))
+        img_resized = resize(img_array,(150,150,3))
+        flat_data.append(img_resized.flatten())
+        images.append(img_resized)
+        target.append(class_num)
+
+flat_data = np.array(flat_data)
+target = np.array(target)
+images = np.array(images)
+
 model = load_model('model/keras_model.h5')
+                
+#Split data into Training and testing
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(flat_data,target,test_size=0.3,random_state=109)
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+from sklearn.model_selection import GridSearchCV
+from sklearn import svm
+param_grid = [
+              {'C':[1,10,100,1000],'kernel':['linear']},
+              {'C':[1,10,100,1000],'gamma':[0.001,0.0001],'kernel':['rbf']},    
+]
+svc = svm.SVC(probability=True)
+clf = GridSearchCV(svc,param_grid)
+clf.fit(x_train,y_train)
+
+y_pred = clf.predict(x_test)
+
+from sklearn.metrics import accuracy_score,confusion_matrix
+accuracy_score(y_pred,y_test)
+confusion_matrix(y_pred,y_test)
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
 	img = Image.open(uploaded_file)
 	st.image(img,caption='Uploaded Image')
 
-# # Load the model
-# model = load_model('keras_model.h5')
-
-# Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-# Load the image
-# img_path = input('Enter the path to your image: ')
-# img = imread(img_path)
-
-# Resize the image
-img_resized = resize(img, (224, 224, 3))
-
-# Expand dimensions to add the batch size
-img_resized = np.expand_dims(img_resized, axis=0)
-
-# Make prediction
-y_out = model.predict(img_resized)
-y_out = np.argmax(y_out, axis=1)
-
-# Map the prediction to the respective class
-class_names = ['Non Venomous', 'Venomous']
-predicted_class = class_names[y_out[0]]
-
-# Display the image
-print("ABCD")
-plt.imshow(img_resized[0])
-plt.show()
-print(f'PREDICTED OUTPUT: {predicted_class}')
+	if st.button('PREDICT'):
+		img_resized = resize(img,(150,150,3))
+		flat_data.append(img_resized.flatten())
+		flat_data = np.array(flat_data)
+		print(img.shape)
+		plt.imshow(img_resized)
+		y_out = model.predict(flat_data)
+		y_out = CATEGORIES[y_out[0]]
+		print(f' PREDICTED OUTPUT: {y_out}')
