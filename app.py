eeede53cc784e98246beb969b67883bedb605960
Mohammad Ikmal <111510512+MalSpielplatz@users.noreply.github.com>
2023-06-11 19:11:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array, save_im
 import numpy as np
 import shutil
 
-import os # inbuilt module
-import random # inbuilt module
-import webbrowser # inbuilt module
+import os
+import random
+import webbrowser
 
 #=================================== Title ===============================
 st.title("""
@@ -25,26 +25,26 @@ st.write("""
 img_file_buffer = st.file_uploader("Upload an image here 👇🏻")
 
 try:
-	image = Image.open(img_file_buffer)
-	img_array = np.array(image)
-	st.write("""
-		Preview 👀 Of Given Image!
-		""")
-	if image is not None:
-	    st.image(
-	        image,
-	        use_column_width=True
-	    )
-	st.write("""
-		Now, you are just one step ahead of prediction.
-		""")
-	st.write("""
-		**Just Click The '👉🏼 Predict' Button To See The Prediction Corresponding To This Image! 😄**
-		""")
+    image = Image.open(img_file_buffer)
+    img_array = np.array(image)
+    st.write("""
+        Preview 👀 Of Given Image!
+        """)
+    if image is not None:
+        st.image(
+            image,
+            use_column_width=True
+        )
+    st.write("""
+        Now, you are just one step ahead of prediction.
+        """)
+    st.write("""
+        **Just Click The '👉🏼 Predict' Button To See The Prediction Corresponding To This Image! 😄**
+        """)
 except:
-	st.write("""
-		### ❗ Any Picture hasn't selected yet!!!
-		""")
+    st.write("""
+        ### ❗ Any Picture hasn't selected yet!!!
+        """)
 
 #================================= Predict Button ============================
 st.text("""""")
@@ -53,47 +53,46 @@ submit = st.button("👉🏼 Predict")
 #==================================== Model ==================================
 def processing(testing_image_path):
     IMG_SIZE = 50
-    img = load_img(testing_image_path, 
-            target_size=(IMG_SIZE, IMG_SIZE), color_mode="grayscale")
+    img = load_img(testing_image_path,
+                   target_size=(IMG_SIZE, IMG_SIZE), color_mode="grayscale")
     img_array = img_to_array(img)
-    img_array = img_array/255.0
-    img_array = img_array.reshape((1, 50, 50, 1))   
-    prediction = model_path_h5.predict(img_array)    
+    img_array = img_array / 255.0
+    img_array = img_array.reshape((1, 50, 50, 1))
+    prediction = model.predict(img_array)
     return prediction
 
-def generate_result(prediction):
-	# Make prediction
-	y_out = model_path_h5.predict(img_resized)
-	y_out = np.argmax(y_out, axis=1)
 
-	# Map the prediction to the respective class
-	class_names = ['Non Venomous', 'Venomous']
-	prediction = class_names[y_out[0]]
-	return prediction
-	
+def generate_result(prediction):
+    class_names = ['Non Venomous', 'Venomous']
+    prediction = class_names[np.argmax(prediction)]
+    return prediction
+
 
 #=========================== Predict Button Clicked ==========================
 if submit:
-	try:
-		# save image on that directory
-		save_img("temp_dir/test_image.png", img_array)
+    try:
+        # save image on that directory
+        if not os.path.exists("temp_dir"):
+            os.makedirs("temp_dir")
 
-		image_path = "temp_dir/test_image.png"
-		# Predicting
-		st.write("👁️ Predicting...")
+        save_img("temp_dir/test_image.png", img_array)
 
-		model_path_h5 = "model/model.h5"
+        image_path = "temp_dir/test_image.png"
+        # Predicting
+        st.write("👁️ Predicting...")
 
-		model_path_h5.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        model_path_h5 = "model/model.h5"
+        model = tf.keras.models.load_model(model_path_h5)
 
-		prediction = processing(image_path)
+        prediction = processing(image_path)
+        result = generate_result(prediction)
 
-		generate_result(prediction)
+        st.write(f"Prediction: {result}")
 
-	except:
-		st.write("""
-		### ❗ Oops... Something Is Going Wrong
-			""")
+    except:
+        st.write("""
+        ### ❗ Oops... Something Is Going Wrong
+        """)
 
 #=============================== Copy Right ==============================
 st.text("""""")
@@ -101,4 +100,4 @@ st.text("""""")
 st.text("""""")
 st.write("""
 ### ©️ Created By awokwaokaowkwao
-	""")
+    """)
